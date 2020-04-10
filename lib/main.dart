e2b9be7d50f528211ba89b3b5b7dd2fe1ef51a1f
Samuel Alapakristi <samalapsy@gmail.com>
2020-04-10 03:14:44 +0100

    Challenge to create magic ball. Done in less than 10 Minutes. Wow !

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: MagicApp(),
+        ),
       ),
     );
+
+class MagicApp extends StatefulWidget {
+  @override
+  _MagicAppState createState() => _MagicAppState();
+}
+
+class _MagicAppState extends State<MagicApp> {
+  int currentQuestion = 1;
+
+  void _askQuestion() {
+    setState(() {
+      currentQuestion = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: _askQuestion,
+              child: Image.asset('images/ball$currentQuestion.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
