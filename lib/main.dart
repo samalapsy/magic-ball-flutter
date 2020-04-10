a9d9c4ef736f4af0b4c41b36fde7eef6f7ecb66c
Samuel Alapakristi <samalapsy@gmail.com>
2020-04-10 03:34:07 +0100

    Customized it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -32,9 +33,26 @@ class _MagicAppState extends State<MagicApp> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
+          SizedBox(
+            height: 50.0,
+          ),
           Expanded(
+            flex: 1,
+            child: Text(
+              'Push the button below to ask',
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0,
+                  color: Colors.white),
+            ),
+          ),
+          Expanded(
+            flex: 15,
             child: FlatButton(
               onPressed: _askQuestion,
               child: Image.asset('images/ball$currentQuestion.png'),
